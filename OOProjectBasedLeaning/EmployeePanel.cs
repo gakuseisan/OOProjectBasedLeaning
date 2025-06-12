fe6cc377277ed1ba4e93a3ccd3a5820a8f1ada8a
宏平 杉山 <c3148@oic.jp>
2025-06-12 15:48:45 +0900

    EditMode.cs追加

--- OOProjectBasedLeaning/EmployeePanel.cs
+++ OOProjectBasedLeaning/EmployeePanel.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace OOProjectBasedLeaning
 {
@@ -40,6 +42,7 @@ namespace OOProjectBasedLeaning
 
             Controls.Add(employeeNameLabel);
             Controls.Add(guestNameTextBox);
+
 
         }
 
