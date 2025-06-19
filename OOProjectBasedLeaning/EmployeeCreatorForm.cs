d39e513d798b89705a4502412f707886bba9a75a
出雲 北島 <C3095@oic.jp>
2025-06-19 14:11:28 +0900

    unchi

--- OOProjectBasedLeaning/EmployeeCreatorForm.cs
+++ OOProjectBasedLeaning/EmployeeCreatorForm.cs
@@ -11,7 +11,7 @@ using System.Windows.Forms;
 namespace OOProjectBasedLeaning
 {
 
-    public partial class EmployeeCreatorForm : Form
+    public partial class EmployeeCreatorForm : DragDropForm
     {
 
         private int employeeId = 10000;
@@ -40,6 +40,24 @@ namespace OOProjectBasedLeaning
             employeeId++;
 
             return new EmployeeModel(employeeId, "Employee" + employeeId);
+
+        }
+        protected override void OnFormDragEnterSerializable(DragEventArgs dragEventArgs)
+        {
+
+            dragEventArgs.Effect = DragDropEffects.Move;
+
+        }
+
+        protected override void OnFormDragDropSerializable(object? serializableObject, DragEventArgs dragEventArgs)
+        {
+
+            if (serializableObject is DragDropPanel)
+            {
+
+                (serializableObject as DragDropPanel).AddDragDropForm(this, PointToClient(new Point(dragEventArgs.X, dragEventArgs.Y)));
+
+            }
 
         }
 
