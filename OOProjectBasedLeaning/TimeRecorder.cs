3284d2221d5db5935060ec30c75fd4a6da52db4d
C3015@oic.osaka.jp <C3015@oic.osaka.jp>
2025-06-26 14:12:35 +0900

    TimeRecorder修正加筆＆実用例記載

--- OOProjectBasedLeaning/TimeRecorder.cs
+++ OOProjectBasedLeaning/TimeRecorder.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OOProjectBasedLeaning;
+using System;
+using System.Text;
 using System.Windows.Forms;
 
 namespace OOProjectBasedLeaning
@@ -16,7 +18,7 @@ namespace OOProjectBasedLeaning
         public TimeRecorder()
         {
             timer = new System.Windows.Forms.Timer();
-            timer.Interval = 1000; // 1秒ごと
+            timer.Interval = 1000; // 1秒
             timer.Tick += OnTimedEvent;
 
             UpdateTime();
@@ -47,3 +49,37 @@ namespace OOProjectBasedLeaning
         }
     }
 }
+
+//前提宣言
+//private TimeRecorder recorder;
+//recorder.start();
+
+
+
+//ラベルの書き換えコード実例
+//labelActionTime.Text = $"{recorder.CurrentDate} {recorder.CurrentTime}";
+
+/*時計の実装例
+private TimeRecorder recorder;
+
+private void Form1_Load(object sender, EventArgs e)
+{
+    recorder = new TimeRecorder();
+
+    recorder.TimeUpdated += (time) =>
+    {
+        labelClock.Text = time;
+    };
+
+    recorder.DateUpdated += (date) =>
+    {
+        labelDate.Text = date;
+    };
+
+    recorder.Start();
+}
+
+private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+{
+    recorder.Dispose();
+}　*/
