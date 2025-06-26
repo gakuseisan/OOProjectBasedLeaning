e8b5939787c191c011896ff13166ef41b2dce182
C3015@oic.osaka.jp <C3015@oic.osaka.jp>
2025-06-26 13:48:14 +0900

    TimeRecorder実装

---
+++ OOProjectBasedLeaning/TimeRecorder.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace OOProjectBasedLeaning
+{
+    internal class TimeRecorder : IDisposable
+    {
+        private System.Windows.Forms.Timer timer;
+
+        public string CurrentTime { get; private set; }
+        public string CurrentDate { get; private set; }
+
+        public event Action<string> TimeUpdated;
+        public event Action<string> DateUpdated;
+
+        public TimeRecorder()
+        {
+            timer = new System.Windows.Forms.Timer();
+            timer.Interval = 1000; // 1秒ごと
+            timer.Tick += OnTimedEvent;
+
+            UpdateTime();
+        }
+
+        private void OnTimedEvent(object sender, EventArgs e)
+        {
+            UpdateTime();
+        }
+
+        private void UpdateTime()
+        {
+            DateTime now = DateTime.Now;
+
+            CurrentTime = now.ToString("HH:mm:ss");
+            CurrentDate = now.ToString("yyyy/MM/dd (ddd)");
+
+            TimeUpdated?.Invoke(CurrentTime);
+            DateUpdated?.Invoke(CurrentDate);
+        }
+
+        public void Start() => timer.Start();
+        public void Stop() => timer.Stop();
+
+        public void Dispose()
+        {
+            timer.Dispose();
+        }
+    }
+}
