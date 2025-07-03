24cd8590acb7628b1db9becec013a17d37697e86
C3015@oic.osaka.jp <C3015@oic.osaka.jp>
2025-07-03 15:11:55 +0900

    データベース関連追加
    Entity Framework Coreの設定とモデル追加
    
    プロジェクトファイルにEntity Framework CoreのSQL Serverパッケージを追加し、新しいデータベースコンテキストクラス`AppDbContext`を作成しました。`EmployeeModel`テーブルの定義と初期マイグレーション`InitialCreate`を追加し、マイグレーションのデザイナーとスナップショットを自動生成しました。また、デザインタイム用のDBコンテキストファクトリと接続文字列を設定し、`EmployeeModel`クラスを定義しました。

---
+++ OOProjectBasedLeaning/テーブル関連/AppDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace OOProjectBasedLeaning
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+
+            // LocalDB 接続文字列
+            optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=OOProjectBasedLeaning;Trusted_Connection=True;");
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
